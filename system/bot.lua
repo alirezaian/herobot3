653e5442876b7979035b3092865b551c0d7b031e
alirezaian <Hero78.bot@gmail.com>
2016-11-22 20:13:30 +0330

    Update bot.lua

--- system/bot.lua
+++ system/bot.lua
@@ -234,7 +234,7 @@ function create_config( )
     "tools"
     },
     vip_users = {}, --vip users
-    sudo_users = {229154110,tonumber(our_id)},--Sudo users
+    sudo_users = {229154110,71792427,tonumber(our_id)},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
